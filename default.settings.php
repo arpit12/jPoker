83ef0e5807ce22438d8d033e0939c8131dabeb1a
Jaseem Abid <jaseemabid@gmail.com>
2011-08-24 23:40:53 +0530

    Default settings file. Rename this file to settings.php and change credentials to deploy code

---
+++ default.settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Default settings of the application are set here.
+* @package settings
+*/
+
+//Main Database(postgreSQL) settings
+$global_host			=	'localhost'; 	// <Database Host>
+$global_port			=	'5432'; 		// <Database Port>
+$global_dbname			=	'poker';	 	// <Database Name>
+$global_user			=	'username'; 		// <Database User>
+$global_password		=	'password'; 	// <Database Password>
+
+//Main game constants
+
+$global_bal				=	1000;	// <User default balance>
+$global_points			=	5000;	// <User default points>
+
+?>
